3b1ab6ef88923b83e46a1ede8549c84ccc7770e2
Sergio <sergiotovar9413@hotmail.com>
2020-07-16 15:21:23 -0500

    Caso de Uso No 2

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -18,6 +18,24 @@ const getProducts = (req, res) => {
     }
 };
 
+const getProduct = (req, res) => {
+    if (req.headers.auth === "admin") {
+        const product = getConnection().get("products").find({ id: Number(req.params.id) }).value();
+        res.json(product);
+    } else {
+        if (req.headers.auth === "customer") {
+            res.status(403).send({
+                error: 'autenticacion incorrecta'
+            });
+        } else {
+            res.status(401).send({
+                error: 'denegado'
+            });
+        }
+    }
+};
+
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct
 }
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,10 +3,11 @@ const router = Router();
 
 const {
     getProducts,
+    getProduct
 } = require("../controllers/products.controllers")
 
 router.get("/products", getProducts);
-router.get("/products/:id", );
+router.get("/products/:id", getProduct);
 router.post("/coupons", );
 router.get("/coupons", );
 router.get("/coupons/:id", );
